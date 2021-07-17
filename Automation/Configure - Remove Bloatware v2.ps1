871fa3a126bc68dcd8e9b05054745e1fa8a688b3
keenits <86823273+keenits@users.noreply.github.com>
2021-07-17 09:53:09 -0700

    Create Configure - Remove Bloatware v2.ps1

---
+++ Automation/Configure - Remove Bloatware v2.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = 'SilentlyContinue'
+
+Write-Output "Removing Most Provisioned Apps - Keep Basic Tools"
+Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -notlike "*windows*"} |Where-Object {$_.PackageName -notlike "*store*"} | Remove-AppxProvisionedPackage -Online
+
+Write-output "Removing Most Apps - Keep Basic Tools"
+Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -notlike "*windows*"} |Where-Object {$_.PackageFullName -notlike "*store*"} |Remove-AppxPackage
