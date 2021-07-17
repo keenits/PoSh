b9d1b1fa937b38fdd70de8a18e48d38ddb73b87f
keenits <86823273+keenits@users.noreply.github.com>
2021-07-17 13:08:10 -0700

    Update Configure - Remove Bloatware v2.ps1

--- Automation/Configure - Remove Bloatware v2.ps1
+++ Automation/Configure - Remove Bloatware v2.ps1
@@ -2,8 +2,6 @@ $ErrorActionPreference = 'SilentlyContinue'
 
 Write-Output "Removing Most Provisioned Apps - Keep Basic Tools"
 Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -notlike "*windows*"} | Where-Object {$_.PackageName -notlike "*store*"} | Remove-AppxProvisionedPackage -Online
-Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -like "*windowscommunicationsapps*"} | Where-Object {$_.PackageName -like "*feedbackhub*"} | Remove-AppxProvisionedPackage -Online
 
 Write-output "Removing Most Apps - Keep Basic Tools"
 Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -notlike "*windows*"} | Where-Object {$_.PackageFullName -notlike "*store*"} | Remove-AppxPackage
-Get-AppxPackage -AllUsers  | Where-Object {$_.PackageFullName -like "*windowscommunicationsapps*"} | Where-Object {$_.PackageFullName -like "*feedbackhub*"} | Remove-AppxPackage
