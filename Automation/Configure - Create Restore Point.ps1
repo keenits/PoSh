7316dec16d1ffb27089f0fbe492a399710aa56fd
keenits <86823273+keenits@users.noreply.github.com>
2021-07-27 16:54:16 -0700

    Update Configure - Create Restore Point.ps1

--- Automation/Configure - Create Restore Point.ps1
+++ Automation/Configure - Create Restore Point.ps1
@@ -1,3 +1,3 @@
-Write-Host "Creating Restore Point in case something bad happens"
+Write-Output "Creating Restore Point in case something bad happens"
 Enable-ComputerRestore -Drive "C:\"
 Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
