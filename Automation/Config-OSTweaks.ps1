599614a4358b61a8064e8bf550b125e0c29853fb
keenits <86823273+keenits@users.noreply.github.com>
2021-07-27 19:31:24 -0700

    Update Config-OSTweaks.ps1

--- Automation/Config-OSTweaks.ps1
+++ Automation/Config-OSTweaks.ps1
@@ -1,6 +1,8 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-Write-Output "Essenital Tweaks Started"
+    Write-Output "Let the tweaking begin"
+    Write-Output "**********************"
+
 #Essential Tweaks
     Write-Output "Disabling Telemetry..."
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
@@ -65,7 +67,7 @@ Write-Output "Essenital Tweaks Started"
     Set-Service "SysMain" -StartupType Disabled
     Write-Output "Setting BIOS time to UTC..."
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" -Name "RealTimeIsUniversal" -Type DWord -Value 1
-    if ((Get-WmiObject -Class Win32_ComputerSystem).PCSystemType -ne 2) #NOT a laptop
+    if ((Get-WmiObject -Class Win32_ComputerSystem).PCSystemType -ne 2)
     {Write-Output "Disabling Hibernation..."
     Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Session Manager\Power" -Name "HibernteEnabled" -Type Dword -Value 0
     If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings")) {
@@ -77,28 +79,53 @@ Write-Output "Essenital Tweaks Started"
         New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" | Out-Null
     }
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FlyoutMenuSettings" -Name "ShowSleepOption" -Type Dword -Value 0
-    powercfg.exe -change -monitor-timeout-ac 0
+    powercfg.exe -change -monitor-timeout-ac 15
     powercfg.exe -change -standby-timeout-ac 0
     Write-Output "Hiding 3D Objects icon from This PC..."
     Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}" -Recurse -ErrorAction SilentlyContinue
-
-    # Network Tweaks
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "IRPStackSize" -Type DWord -Value 20
-
-    # SVCHost Tweak
-    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value 4194304
-
-    Write-Output "Disable News and Interests"
+    Write-Output "Disabling News and Interests..."
     If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds")) {
         New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" -Force | Out-Null
         New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" -Name "EnableFeeds" -PropertyType DWord -Value 0 -Force | Out-Null
     }
+    Write-Output "Disabling Cortana..."
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+    If (!(Test-Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore")) {
+        New-Item -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+    
+#Network Tweaks
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" -Name "IRPStackSize" -Type DWord -Value 20
 
-    Write-Output "Essential Tweaks Completed"
+#SVCHost Tweak
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name "SvcHostSplitThresholdInKB" -Type DWord -Value 4194304
 
-#Hovens tweaks
-    Write-Output "Hovens Tweaks Started"
+#More Tweaks
+    Write-Output "Setting TimeZone..."
+    Set-TimeZone -Name "Pacific Standard Time"
 
+    Write-Output "Disabling Windows PIN..."
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\PassportForWork" /v Enabled /t REG_DWORD /d 0 /f | Out-Null
+
+    Write-Output "Disabling MS Teams auto start..."
+    reg add "HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Run" /v "TeamsMachineInstaller" /t REG_SZ /d '-' /f | Out-Null
+    
+    Write-Output "Disabling last logged on user list..."
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v dontdisplaylastusername /t REG_DWORD /d 1 /f | Out-Null
+    
     Write-Output "Configuring Edge settings..."
     reg add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v CreateDesktopShortcutDefault /t REG_DWORD /d 0 /f | Out-Null
     reg add "HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\Main" /v PreventFirstRunPage /t REG_DWORD /d 1 /f | Out-Null
@@ -107,17 +134,24 @@ Write-Output "Essenital Tweaks Started"
     reg add "HKLM\SOFTWARE\Microsoft\Edge" /v HideFirstRunExperience /t REG_DWORD /d 1 /f | Out-Null
     reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v DisableEdgeDesktopShortcutCreation /t REG_DWORD /d 1 /f | Out-Null
 
-    Write-Output "Setting TimeZone..."
-    Set-TimeZone -Name "Pacific Standard Time"
-
     Write-Output "Disabling first logon privacy settings..."
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\OOBE" /v DisablePrivacyExperience /t REG_DWORD /d 1 /f | Out-Null
 
     Write-Output "Disabling first logon animation..."
     reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableFirstLogonAnimation /t REG_DWORD /d 0 /f | Out-Null
 
+    Write-Output "Disabling the use of Microsoft accounts..."
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v NoConnectedUser /d 3 /t REG_DWORD /f | Out-Null
+
+    Write-Output "Disabling automatic setup of network connected devices..."
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\NcdAutoSetup\Private" /v AutoSetup /t REG_DWORD /d 0 /f | Out-Null
+
     Write-Output "Disabling recently added apps on start menu..."
     reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v HideRecentlyAddedApps /t REG_DWORD /d 1 /f | Out-Null
+
+    Write-Output "Disabling unwanted store apps..."
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v DisableWindowsConsumerFeatures /d 1 /t REG_DWORD /f | Out-Null
+    reg add "HKLM\SOFTWARE\Policies\Microsoft\WindowsStore" /v AutoDownload /t REG_DWORD /d 2 /f | Out-Null
 
     Write-Output "Renaming system drive..."
     Set-Volume -DriveLetter C -NewFileSystemLabel "Windows"
@@ -129,10 +163,22 @@ Write-Output "Essenital Tweaks Started"
     Set-ItemProperty -path "HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters" EnableLMHOSTS -value 0
 
     Write-Output "Configuring default app associations..."
-    $download = "https://github.com/keenits/PoSh/blob/a4338ca848e114e9b40cca9486a1ec662dfd1706/Automation/Files/defaultassociations.xml"
+    $download = "https://raw.githubusercontent.com/keenits/PoSh/main/Automation/Files/defaultassociations.xml"
     $output = "C:\Windows\System32\defaultassociations.xml"
     Invoke-RestMethod -Uri $download -OutFile $output
     Dism /online /import-defaultappassociations:C:\Windows\System32\defaultassociations.xml
+
+    Write-Output "Deleting desktop shortcuts"
+    Try
+    {
+        Remove-Item 'C:\Users\Public\Desktop\Acrobat Reader DC.lnk'
+        Remove-Item 'C:\Users\Public\Desktop\Google Chrome.lnk'
+        Remove-Item 'C:\Users\Public\Desktop\Microsoft Edge.lnk'
+    }
+    Catch
+    {}
+
+#    schtasks /change /tn "\Microsoft\Windows\Workplace Join\Automatic-Device-Join" /disable
 
 #Function - Force NetBIOS over TCP/IP
     Function SetTCPIP {$adapters=(gwmi win32_networkadapterconfiguration )
@@ -146,5 +192,6 @@ Write-Output "Essenital Tweaks Started"
     Write-Output "Executing function calls..."
     SetTCPIP | Out-Null
     ReletterDrive | Out-Null
-    
-    Write-Output "Hovens Tweaks Completed"
+
+    Write-Output "***********************************"
+    Write-Output "Successfully implemented all tweaks"
