60cbcb1f2921cd40ac2daea839eb12f15ca0f435
keenits <86823273+keenits@users.noreply.github.com>
2021-07-30 05:45:33 -0700

    Delete Config-RestorePoint.ps1

--- Automation/Config-RestorePoint.ps1
+++
@@ -1,3 +0,0 @@
-Write-Output "Creating Restore Point in case something bad happens"
-Enable-ComputerRestore -Drive "C:\"
-Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
